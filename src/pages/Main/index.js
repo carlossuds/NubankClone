8b422ae940d54155e540fd0b7809c39eea7e22bd
Carlos Sudsilowsky <sudsilowsky@gmail.com>
2020-01-22 19:24:22 -0300

    First commit

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {Animated} from 'react-native';
+import {PanGestureHandler, State} from 'react-native-gesture-handler';
 
 import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
 import Menu from '~/components/Menu';
-
-import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import {
   Container,
@@ -16,31 +19,62 @@ import {
   CardFooter,
   Annotation,
 } from './styles';
-import Tabs from '~/components/Tabs';
 
 export default function Main() {
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    {
+      useNativeDriver: true,
+    },
+  );
+
+  function onHandlerStateChanged(event) {}
+
   return (
     <Container>
       <Header />
 
       <Content>
-        <Menu />
+        <Menu translateY={translateY} />
 
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description>R$ 304.213.838,63</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 200,00 recebida de Igor Sebulbax às 23:59h
-            </Annotation>
-          </CardFooter>
-        </Card>
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}>
+          <Card
+            style={{
+              transform: [
+                {
+                  translateY: translateY.interpolate({
+                    inputRange: [0, 380],
+                    outputRange: [0, 380],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            }}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 304.213.838,63</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 200,00 recebida de Igor Sebulbax às 23:59h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
 
       <Tabs />
